9a2263428c3379e5ded00f86a5f91e9533980b9c
Daniël <daniel@lngzl.nl>
2022-02-20 22:39:57 +0100

    formatted code

--- 1/one.cpp
+++ 1/one.cpp
@@ -2,19 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
-    int input [4] = {10, 15, 3 ,7};
+    int input[4] = {10, 15, 3, 7};
     int k = 17;
 
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            if(input[i] + input[j] == k) {
+            if (input[i] + input[j] == k)
+            {
                 std::cout << input[i] << " and " << input[j] << " makes " << k << std::endl;
                 return 0;
             }
-
-        }        
+        }
     }
-        return 0;
+    return 0;
 }
