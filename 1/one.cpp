50bb027fbcf0b2b87a1206626ee4464620d9a5eb
Daniël <daniel@lngzl.nl>
2022-02-20 18:20:50 +0100

    wip

---
+++ 1/one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int input [5] = {10, 15, 3 ,7};
+    int k = 17;
+
+    for (int i = 0; i < sizeof(input); i++)
+    {
+        for (int j = 0; j < sizeof(input); j++)
+        {
+            std::cout << "What" << std::endl;
+
+        }        
+    }
+        return 0;
+}
