be4a9be62cad246c8cc004ce3ed3b2ca299afc33
Daniël <daniel@lngzl.nl>
2022-02-23 18:23:34 +0100

    e

---
+++ 3/three.cpp
@@ -0,0 +1,15 @@
+// #include <string>
+
+// int main(int argc, char const *argv[])
+// {
+//     class Node
+//     {
+//     public:
+//         std::string val;
+//         Node *left;
+//         Node *right;
+//     };
+//     Node node = new Node('root', new Node('left', new Node('left.left'), new Node('right'))) return 0;
+// }
+
+I'm too new with C++ for this one.
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Daily code problem
-This is the place where I solve code problems.
+This is the place where I solve code problems. For now with C++ as I'm just trying to learn the basics.
 
 Sign up at https://www.dailycodingproblem.com/
+
+## Couldn't complete
+`3`: I'm too new with C++ to understand this one. I'll come back once I understand C++ more.
